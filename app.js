642358bf3a67304b8af9435e2a025e6d11cc51c7
Asfath Ahamed <asfathahamed@gmail.com>
2020-01-18 11:53:00 +0530

    Simple Mongo scripts

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017", function(err, db) {
+    if (err) throw err;
+    dbObj = db.db('TodoData');
+    dbObj.createCollection('TodoList', function(err, collection) {
+        if (err) throw err;
+        console.log('Collection Created');
+        
+        collection.insertOne({
+            id: 1,
+            task: 'My First Task'
+        }, function(err, res) {
+            if (err) throw err;
+            console.log('Inserted')
+        });
+        collection.find({}).toArray(function(err, res) {
+            if (err) throw err;
+            console.log('result', res);
+        })
+    });
+});
+
 var app = express();
 
 app.use(logger('dev'));
